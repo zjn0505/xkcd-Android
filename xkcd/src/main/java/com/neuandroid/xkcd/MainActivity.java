6465f36b7e8f198a42ea1a6a353049fdd7cce403
Jienan Zhang <zjn0505@hotmail.com>
2017-07-09 14:01:54 +0800

    Add the first menu icon for loading random xkcd pic

--- xkcd/src/main/java/com/neuandroid/xkcd/MainActivity.java
+++ xkcd/src/main/java/com/neuandroid/xkcd/MainActivity.java
@@ -3,6 +3,7 @@ package com.neuandroid.xkcd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -87,4 +88,11 @@ public class MainActivity extends AppCompatActivity {
                 renderXkcdPic((XkcdPic) result);
         }
     };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
