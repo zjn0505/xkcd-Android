d7131c90040e72a9b5550952b9c192d6ed168ffe
Jienan Zhang <zjn0505@hotmail.com>
2017-07-10 14:58:25 +0800

    Add some animation

--- xkcd/src/main/java/com/neuandroid/xkcd/activity/ImageDetailPageActivity.java
+++ xkcd/src/main/java/com/neuandroid/xkcd/activity/ImageDetailPageActivity.java
@@ -3,6 +3,7 @@ package com.neuandroid.xkcd.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
@@ -22,5 +23,12 @@ public class ImageDetailPageActivity extends Activity {
 
         PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
         Glide.with(this).load(url).into(photoView);
+        photoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        });
     }
 }
--- xkcd/src/main/java/com/neuandroid/xkcd/activity/MainActivity.java
+++ xkcd/src/main/java/com/neuandroid/xkcd/activity/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, ImageDetailPageActivity.class);
         intent.putExtra("URL", currentPic.img);
         startActivity(intent);
-
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
     }
 
 
