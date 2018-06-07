1f1bb562948909f9317481ef68f121b79ac5e633
Jienan Zhang <zjn0505@hotmail.com>
2018-06-07 22:10:01 +0800

    Update js

--- xkcd/src/main/assets/LatexInterface.js
+++ xkcd/src/main/assets/LatexInterface.js
@@ -3,11 +3,19 @@ window.addEventListener('load', function(){
     Array.from(document.getElementsByClassName('latex')).forEach(
         function(element, index, array) {
             element.addEventListener('touchstart', function(e){
-                AndroidLatex.onTouch(1);
+                if (element.scrollWidth - element.scrollWidth > element.scrollLeft) {
+                    AndroidLatex.onTouch(2)
+                } else {
+                    AndroidLatex.onTouch(3);
+                }
             }, false)
 
             element.addEventListener('touchmove', function(e){
-                AndroidLatex.onTouch(2);
+                if (element.scrollWidth - element.scrollWidth > element.scrollLeft) {
+                    AndroidLatex.onTouch(2)
+                } else {
+                    AndroidLatex.onTouch(3);
+                }
             }, false)
 
             element.addEventListener('touchend', function(e){
