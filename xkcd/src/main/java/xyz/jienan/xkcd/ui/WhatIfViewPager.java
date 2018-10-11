0c2258ddf3a734f0156570e6386f8e8d51ed03ff
Jienan Zhang <zjn0505@hotmail.com>
2018-10-11 16:07:58 +0800

    Apply HackyViewPager for viewPager crash

--- xkcd/src/main/java/xyz/jienan/xkcd/ui/WhatIfViewPager.java
+++ xkcd/src/main/java/xyz/jienan/xkcd/ui/WhatIfViewPager.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class WhatIfViewPager extends ViewPager {
@@ -19,6 +20,16 @@ public class WhatIfViewPager extends ViewPager {
             return ((WhatIfWebView) v).canScrollHor(-dx);
         } else {
             return super.canScroll(v, checkV, dx, x, y);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            // HackyViewPager
+            return false;
         }
     }
 }
