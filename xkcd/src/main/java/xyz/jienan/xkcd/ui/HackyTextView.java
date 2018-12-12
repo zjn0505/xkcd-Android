ddbe9c9d7df9eae200edbe1df6bccfaf410b0170
Jienan Zhang <zjn0505@hotmail.com>
2018-12-12 15:40:39 +0800

    Add HackyTextView to fix text selection crash https://issuetracker.google.com/issues/37020604

---
+++ xkcd/src/main/java/xyz/jienan/xkcd/ui/HackyTextView.java
@@ -0,0 +1,36 @@
+package xyz.jienan.xkcd.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.Selection;
+import android.text.Spannable;
+import android.util.AttributeSet;
+
+public class HackyTextView extends AppCompatTextView {
+
+    public HackyTextView(Context context) {
+        super(context);
+    }
+
+    public HackyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HackyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+        if (selStart == -1 || selEnd == -1) {
+            // @hack : https://issuetracker.google.com/issues/37020604
+            CharSequence text = getText();
+            if (text instanceof Spannable) {
+                Selection.setSelection((Spannable) text, 0, 0);
+            }
+        } else {
+            super.onSelectionChanged(selStart, selEnd);
+        }
+    }
+}
