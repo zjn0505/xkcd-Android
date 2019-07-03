d06a0c223534dd6e24473810cfde039cbc5a9b25
Jienan Zhang <zjn0505@hotmail.com>
2019-07-03 16:50:47 +0800

    Update pref lib

--- xkcd/src/main/java/xyz/jienan/xkcd/settings/SettingsFragment.kt
+++ xkcd/src/main/java/xyz/jienan/xkcd/settings/SettingsFragment.kt
@@ -1,10 +1,10 @@
 package xyz.jienan.xkcd.settings
 
 import android.os.Bundle
-import android.preference.ListPreference
-import android.preference.Preference
-import android.preference.PreferenceFragment
-import android.preference.SwitchPreference
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import xyz.jienan.xkcd.Const.*
 import xyz.jienan.xkcd.R
 import xyz.jienan.xkcd.model.WhatIfModel
@@ -13,58 +13,58 @@ import xyz.jienan.xkcd.model.WhatIfModel
  * Created by Jienan on 2018/3/9.
  */
 
-class SettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
+class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
 
-    private val arrowPref by lazy { findPreference(PREF_ARROW) as ListPreference }
-    private val randomPref by lazy { findPreference(PREF_RANDOM) as ListPreference }
-    private val zoomPref by lazy { findPreference(PREF_ZOOM) as ListPreference }
-    private val searchPref by lazy { findPreference(PREF_WHAT_IF_SEARCH) as ListPreference }
+    private val arrowPref by lazy { findPreference<ListPreference>(PREF_ARROW) }
+    private val randomPref by lazy { findPreference<ListPreference>(PREF_RANDOM) }
+    private val zoomPref by lazy { findPreference<ListPreference>(PREF_ZOOM) }
+    private val searchPref by lazy { findPreference<ListPreference>(PREF_WHAT_IF_SEARCH) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.prefs)
-        findPreference(PREF_FONT).onPreferenceChangeListener = this
-        findPreference(PREF_XKCD_GIF_ECO).onPreferenceChangeListener = this
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.prefs, rootKey)
 
-        arrowPref.summary = resources.getQuantityString(R.plurals.pref_arrow_summary,
-                Integer.valueOf(arrowPref.entry.toString()), arrowPref.entry.toString())
-        arrowPref.onPreferenceChangeListener = this
+        findPreference<SwitchPreferenceCompat>(PREF_FONT)?.onPreferenceChangeListener = this
+        findPreference<SwitchPreferenceCompat>(PREF_XKCD_GIF_ECO)?.onPreferenceChangeListener = this
 
-        randomPref.summary = randomPref.entry.toString()
-        randomPref.onPreferenceChangeListener = this
+        arrowPref?.summary = resources.getQuantityString(R.plurals.pref_arrow_summary,
+                Integer.valueOf(arrowPref?.entry.toString()), arrowPref?.entry.toString())
+        arrowPref?.onPreferenceChangeListener = this
 
-        searchPref.summary = searchPref.entry.toString()
-        searchPref.onPreferenceChangeListener = this
+        randomPref?.summary = randomPref?.entry.toString()
+        randomPref?.onPreferenceChangeListener = this
 
-        zoomPref.summary = zoomPref.entry.toString()
-        zoomPref.onPreferenceChangeListener = this
+        searchPref?.summary = searchPref?.entry.toString()
+        searchPref?.onPreferenceChangeListener = this
+
+        zoomPref?.summary = zoomPref?.entry.toString()
+        zoomPref?.onPreferenceChangeListener = this
     }
 
-    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
-        when (preference.key) {
+    override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+        when (preference?.key) {
             PREF_ARROW -> {
                 (preference as ListPreference).value = newValue.toString()
-                arrowPref.summary = resources.getQuantityString(R.plurals.pref_arrow_summary,
-                        Integer.valueOf(arrowPref.entry.toString()), arrowPref.entry.toString())
+                arrowPref?.summary = resources.getQuantityString(R.plurals.pref_arrow_summary,
+                        Integer.valueOf(arrowPref?.entry.toString()), arrowPref?.entry.toString())
             }
             PREF_RANDOM -> {
                 (preference as ListPreference).value = newValue.toString()
-                randomPref.summary = randomPref.entry.toString()
+                randomPref?.summary = randomPref?.entry.toString()
             }
             PREF_FONT -> {
-                (preference as SwitchPreference).isChecked = newValue as Boolean
-                activity.recreate()
+                (preference as SwitchPreferenceCompat).isChecked = newValue as Boolean
+                activity?.recreate()
             }
             PREF_ZOOM -> {
                 (preference as ListPreference).value = newValue.toString()
-                zoomPref.summary = zoomPref.entry.toString()
+                zoomPref?.summary = zoomPref?.entry.toString()
                 WhatIfModel.setZoom(Integer.valueOf(newValue.toString().substring(5)))
             }
             PREF_WHAT_IF_SEARCH -> {
                 (preference as ListPreference).value = newValue.toString()
-                searchPref.summary = searchPref.entry.toString()
+                searchPref?.summary = searchPref?.entry.toString()
             }
-            PREF_XKCD_GIF_ECO -> (preference as SwitchPreference).isChecked = newValue as Boolean
+            PREF_XKCD_GIF_ECO -> (preference as SwitchPreferenceCompat).isChecked = newValue as Boolean
         }
         return false
     }
