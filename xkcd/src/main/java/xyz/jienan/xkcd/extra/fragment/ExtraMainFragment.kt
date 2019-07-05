5108370550b6b050b51e0a1c8aee0248b044db9c
Jienan Zhang <zjn0505@hotmail.com>
2019-07-05 11:20:48 +0800

    Optimize Extra

--- xkcd/src/main/java/xyz/jienan/xkcd/extra/fragment/ExtraMainFragment.kt
+++ xkcd/src/main/java/xyz/jienan/xkcd/extra/fragment/ExtraMainFragment.kt
@@ -1,20 +1,17 @@
 package xyz.jienan.xkcd.extra.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.fab_sub_icons.*
 import kotlinx.android.synthetic.main.fragment_comic_main.*
-import xyz.jienan.xkcd.Const.FIRE_BROWSE_LIST_MENU
 import xyz.jienan.xkcd.Const.LAST_VIEW_XKCD_ID
 import xyz.jienan.xkcd.R
 import xyz.jienan.xkcd.extra.ExtraPagerAdapter
 import xyz.jienan.xkcd.extra.contract.ExtraMainContract
 import xyz.jienan.xkcd.extra.presenter.ExtraMainPresenter
 import xyz.jienan.xkcd.home.base.ContentMainBaseFragment
-import xyz.jienan.xkcd.list.activity.XkcdListActivity
 import xyz.jienan.xkcd.model.ExtraComics
 
 class ExtraMainFragment : ContentMainBaseFragment(), ExtraMainContract.View {
@@ -63,17 +60,6 @@ class ExtraMainFragment : ContentMainBaseFragment(), ExtraMainContract.View {
         super.onPrepareOptionsMenu(menu)
         menu.findItem(R.id.action_go_xkcd).isVisible = false
         menu.findItem(R.id.action_search).isVisible = false
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_xkcd_list -> {
-                val intent = Intent(activity, XkcdListActivity::class.java)
-                startActivityForResult(intent, REQ_LIST_ACTIVITY)
-                logUXEvent(FIRE_BROWSE_LIST_MENU)
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun showExtras(extraComics: List<ExtraComics>) {
--- xkcd/src/main/java/xyz/jienan/xkcd/extra/fragment/SingleExtraFragment.kt
+++ xkcd/src/main/java/xyz/jienan/xkcd/extra/fragment/SingleExtraFragment.kt
@@ -10,7 +10,6 @@ import android.view.HapticFeedbackConstants.LONG_PRESS
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestListener
