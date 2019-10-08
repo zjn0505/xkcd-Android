936817054dc3a539e4dd61481a29c702cc9dcaad
Jienan Zhang <zjn0505@hotmail.com>
2019-10-08 23:02:39 +0800

    Add xkcd notification worker

--- xkcd/src/foss/java/xyz/jienan/xkcd/FlavorUtils.kt
+++ xkcd/src/foss/java/xyz/jienan/xkcd/FlavorUtils.kt
@@ -1,19 +1,22 @@
 package xyz.jienan.xkcd
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.*
 import androidx.work.PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import xyz.jienan.xkcd.base.NotificationWorker
 import java.util.concurrent.TimeUnit
 
 object FlavorUtils {
     fun init(app: Application) {
-        val notificationWorkRequest = PeriodicWorkRequestBuilder<NotificationWorker>(MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS).addTag("UpdateXkcd").build()
+        val notificationWorkRequest = PeriodicWorkRequestBuilder<NotificationWorker>(MIN_PERIODIC_INTERVAL_MILLIS,
+                TimeUnit.MILLISECONDS)
+                .addTag("Update")
+                .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+                .build()
         WorkManager.getInstance(app)
-                .enqueueUniquePeriodicWork("UpdateXkcd", ExistingPeriodicWorkPolicy.KEEP, notificationWorkRequest)
-        WorkManager.getInstance(app).cancelAllWork()
+                .enqueueUniquePeriodicWork("UpdateXkcd",
+                        ExistingPeriodicWorkPolicy.KEEP,
+                        notificationWorkRequest)
     }
 
     fun updateLocale() {
--- xkcd/src/foss/java/xyz/jienan/xkcd/base/NotificationWorker.kt
+++ xkcd/src/foss/java/xyz/jienan/xkcd/base/NotificationWorker.kt
@@ -3,10 +3,15 @@ package xyz.jienan.xkcd.base
 import android.content.Context
 import androidx.work.RxWorker
 import androidx.work.WorkerParameters
+import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import xyz.jienan.xkcd.base.network.NetworkService
+import xyz.jienan.xkcd.model.XkcdPic
+import xyz.jienan.xkcd.model.persist.BoxManager
+import xyz.jienan.xkcd.model.persist.SharedPrefManager
+import xyz.jienan.xkcd.ui.NotificationUtils
 
 class NotificationWorker(appContext: Context, workerParams: WorkerParameters)
     : RxWorker(appContext, workerParams) {
@@ -15,10 +20,20 @@ class NotificationWorker(appContext: Context, workerParams: WorkerParameters)
         Timber.d("Create work ${this.tags}")
         return NetworkService.xkcdAPI
                 .latest
-                .doOnNext { xkcdPic -> Timber.d("GetXkcdPic $xkcdPic") }
-                .map { Result.success() }
-                .singleOrError()
-                .onErrorReturnItem(Result.failure())
                 .observeOn(Schedulers.io())
+                .doOnNext { xkcdPic -> Timber.d("GetXkcdPic $xkcdPic") }
+                .flatMapCompletable { checkLatestPicAndNotify(it) }
+                .toSingleDefault(Result.success())
+                .onErrorReturnItem(Result.failure())
+    }
+
+    private fun checkLatestPicAndNotify(xkcdPic: XkcdPic) : Completable {
+        return Completable.fromAction {
+            if (SharedPrefManager.latestXkcd < xkcdPic.num) {
+                SharedPrefManager.latestXkcd = xkcdPic.num
+                BoxManager.updateAndSave(xkcdPic)
+                NotificationUtils.showNotification(applicationContext, xkcdPic)
+            }
+        }
     }
 }
