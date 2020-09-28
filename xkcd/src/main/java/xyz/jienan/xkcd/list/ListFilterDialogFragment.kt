7f292f2d94c965754da2dcb564cdb82e65e6c933
Jienan Zhang <zjn0505@hotmail.com>
2020-09-29 00:24:53 +0800

    Update list filter

--- xkcd/src/main/java/xyz/jienan/xkcd/list/ListFilterDialogFragment.kt
+++ xkcd/src/main/java/xyz/jienan/xkcd/list/ListFilterDialogFragment.kt
@@ -10,13 +10,25 @@ import android.widget.BaseAdapter
 import android.widget.CheckedTextView
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import xyz.jienan.xkcd.R
+import xyz.jienan.xkcd.list.activity.BaseListActivity
 import java.util.*
 
-class ListFilterDialogFragment : DialogFragment() {
+class ListFilterDialogFragment() : DialogFragment() {
 
-    private var list: MutableList<HashMap<String, String>> = mutableListOf()
+    companion object {
+        private const val KEY_HAS_FAV = "hasFav"
+
+        fun newInstance(hasFav: Boolean): ListFilterDialogFragment {
+            val fragment = ListFilterDialogFragment()
+            fragment.arguments = bundleOf(KEY_HAS_FAV to hasFav)
+            return fragment
+        }
+    }
+
+    private var list: MutableList<HashMap<String, Any>> = mutableListOf()
 
     var selection: Int = 0
 
@@ -24,19 +36,40 @@ class ListFilterDialogFragment : DialogFragment() {
 
     var filters = intArrayOf()
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(activity!!)
-        val icons = intArrayOf(R.mipmap.ic_launcher_round, R.drawable.ic_heart_on, R.drawable.ic_thumb_on)
-        for (i in filters.indices) {
-            val hashMap = hashMapOf<String, String>()
-            hashMap["filter"] = getString(filters[i])
-            hashMap["iconRes"] = icons[i].toString()
-            list.add(hashMap)
+    private val hasFav: Boolean
+        get() = arguments?.getBoolean(KEY_HAS_FAV) != false
+
+    private val icons: IntArray by lazy {
+        if (hasFav) {
+            intArrayOf(R.mipmap.ic_launcher_round, R.drawable.ic_heart_on, R.drawable.ic_thumb_on)
+        } else {
+            intArrayOf(R.mipmap.ic_launcher_round, R.drawable.ic_thumb_on)
         }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireActivity())
+
+        filters.filterIndexed { index, i -> hasFav || index != 1 }.forEachIndexed { index, res ->
+            list.add(hashMapOf("filter" to getString(res), "iconRes" to icons[index]))
+        }
+
         val adapter = FilterAdapter()
 
         builder.setAdapter(adapter) { dialog, which ->
-            itemSelectListener?.onItemSelected(which)
+            val selection = if (hasFav) {
+                when (which) {
+                    0 -> BaseListActivity.Selection.ALL
+                    1 -> BaseListActivity.Selection.MY_FAVORITE
+                    else -> BaseListActivity.Selection.PEOPLES_CHOICE
+                }
+            } else {
+                when (which) {
+                    0 -> BaseListActivity.Selection.ALL
+                    else -> BaseListActivity.Selection.PEOPLES_CHOICE
+                }
+            }
+            itemSelectListener?.onItemSelected(selection)
             dialog.dismiss()
         }
 
@@ -44,7 +77,7 @@ class ListFilterDialogFragment : DialogFragment() {
     }
 
     interface OnItemSelectListener {
-        fun onItemSelected(which: Int)
+        fun onItemSelected(selection: BaseListActivity.Selection)
     }
 
     private inner class FilterAdapter : BaseAdapter() {
@@ -61,9 +94,9 @@ class ListFilterDialogFragment : DialogFragment() {
             val map = list[position]
             val ivFilter = view.findViewById<ImageView>(R.id.iv_filter)
             val tvFilter = view.findViewById<CheckedTextView>(R.id.tv_filter)
-            ivFilter.setImageResource(Integer.valueOf(map["iconRes"]!!))
+            ivFilter.setImageResource(map["iconRes"]!! as Int)
             tvFilter.isChecked = position == selection
-            tvFilter.text = map["filter"]
+            tvFilter.text = map["filter"].toString()
             return view
         }
     }
