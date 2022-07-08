3664216c4906b1972e0bd58714fcd17275a62b8f
Jienan Zhang <zjn0505@hotmail.com>
2022-07-08 13:43:48 +0800

    Add title to what if when missing.

--- xkcd/src/main/java/xyz/jienan/xkcd/whatif/fragment/SingleWhatIfFragment.kt
+++ xkcd/src/main/java/xyz/jienan/xkcd/whatif/fragment/SingleWhatIfFragment.kt
@@ -197,6 +197,13 @@ open class SingleWhatIfFragment : BaseFragment(), ImgInterface.ImgCallback, RefI
                     if (context?.getUiNightModeFlag() == Configuration.UI_MODE_NIGHT_YES) {
                         val doc = Jsoup.parse(it.content)
                         doc.head()!!.appendCss("night_style.css")
+                        val ele = doc.body().child(0)
+                        Timber.d("Index $index, First Ele, $ele")
+                        if (ele.tag().name == "p" && ele.attr("id") == "question") {
+                            val titleAHref = Element("a").attr("href", "https://what-if.xkcd.com/${it.num}")
+                            titleAHref.appendChild(Element("h1").appendText(it.title))
+                            doc.head().insertChildren(0, titleAHref)
+                        }
                         it.content = doc.html()
                     }
                     it
